9c31313f01316c45db4106a53dc79ea2a836a503
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2025-12-16 15:46:48 +0100

    add ChrisTestPerson class

---
+++ ColabTestProject/ChrisTestPerson.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColabTestProject
+{
+    internal class ChrisTestPerson(string name)
+    {
+        public string Name { get; set; } = name;
+
+        public void PrintName()
+        {
+            Console.WriteLine(Name);
+        }
+    }
+}
