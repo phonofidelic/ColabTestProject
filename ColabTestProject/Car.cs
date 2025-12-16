4a9ec5206e8061da471e909ce6d20714f79ddd5f
Martin Olsson <martinolsson89@gmail.com>
2025-12-16 15:42:58 +0100

    added new class (Car.cs)

---
+++ ColabTestProject/Car.cs
@@ -0,0 +1,8 @@
+﻿namespace ColabTestProject;
+
+internal class Car
+{
+    public string Model { get; set; } = string.Empty;
+    public string Make { get; set; } = string.Empty;
+    public string RegistrationNumber { get; set; } = string.Empty;
+}
